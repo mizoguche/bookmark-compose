c939ddb38c3eaf0312d19b6c7eaddbbfd5913e47
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2021-06-27 16:42:55 +0900

    Use scaffold

--- app/src/main/java/dev/mizoguche/bookmarkcompose/ui/main/MainScreen.kt
+++ app/src/main/java/dev/mizoguche/bookmarkcompose/ui/main/MainScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,13 +20,23 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.BottomNavigation
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -54,28 +65,36 @@ fun MainScreen(
     val articles by viewModel.articles.collectAsState()
     val viewStatus by viewModel.viewStatus.collectAsState()
 
-    Surface(modifier = modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxSize(),
-        ) {
-            SwipeRefresh(
-                state = rememberSwipeRefreshState(
-                    isRefreshing = (viewStatus as? ViewStatus.Ready)?.isRefreshing ?: false
-                ),
-                onRefresh = { viewModel.refresh() },
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Hatebu-compose") },
+            )
+        },
+    ) {
+        Surface(modifier = modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
             ) {
-                Crossfade(targetState = viewStatus) {
-                    when (it) {
-                        is ViewStatus.Ready -> {
-                            Articles(
-                                articles = articles,
-                                modifier = Modifier.fillMaxSize(),
-                            )
+                SwipeRefresh(
+                    state = rememberSwipeRefreshState(
+                        isRefreshing = (viewStatus as? ViewStatus.Ready)?.isRefreshing ?: false
+                    ),
+                    onRefresh = { viewModel.refresh() },
+                ) {
+                    Crossfade(targetState = viewStatus) {
+                        when (it) {
+                            is ViewStatus.Ready -> {
+                                Articles(
+                                    articles = articles,
+                                    modifier = Modifier.fillMaxSize(),
+                                )
+                            }
+                            is ViewStatus.Loading -> ArticlesPlaceholder()
+                            is ViewStatus.Error -> Text(text = "error")
                         }
-                        is ViewStatus.Loading -> ArticlesPlaceholder()
-                        is ViewStatus.Error -> Text(text = "error")
                     }
                 }
             }
